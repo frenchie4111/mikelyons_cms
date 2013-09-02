60f5efb0b6043e5071232ff3122a6cb583dd2fb3
Mike Lyons <mdl0394@gmail.com>
2013-09-01 23:37:13 -0400

    Added Pages editing

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -2,4 +2,16 @@ class Admin::PagesController < Admin::ApplicationController
     def index
         @pages = Page.all
     end
+
+    def edit
+        @page = Page.find( params[:id] )
+    end
+
+    def update
+        Page.find(params[:id]).update_attributes(params[:page].permit(:body, :title))
+
+        respond_to do |format|
+            format.js
+        end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130830140511) do
+ActiveRecord::Schema.define(version: 20130902014444) do
 
   create_table "blog_posts", force: true do |t|
     t.text     "body"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20130830140511) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title"
   end
 
 end
